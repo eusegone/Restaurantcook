1edde394bd758899f613821c644f91a914064095
Antonio <chefeusebio@gmail.com>
2015-03-04 19:46:29 +0000

    working with heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module RestaurantCook
   class Application < Rails::Application
 
+    config.serve_static_assets = true
     config.autoload_paths += %W(#{config.root}/app/uploaders)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
